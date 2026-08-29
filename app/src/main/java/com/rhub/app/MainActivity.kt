bfdaad820c980e9e976bf70c7c5448dfd0d3466b
abdoulacherif <abdoula13cherif@gmail.com>
2026-08-29 07:50:14 +0100

    Create java/com/rhub/app/MainActivity.kt

---
+++ app/src/main/java/com/rhub/app/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.rhub.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import com.rhub.app.ui.navigation.RHubNavGraph
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    RHubNavGraph()
+                }
+            }
+        }
+    }
+}
