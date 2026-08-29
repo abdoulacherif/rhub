95933ee4ee77e9c5c6a5219ed5e0688db89cf085
abdoulacherif <abdoula13cherif@gmail.com>
2026-08-29 07:51:03 +0100

    Create SupabaseClient.kt

---
+++ app/src/main/java/com/rhub/app/SupabaseClient.kt
@@ -0,0 +1,19 @@
+package com.rhub.app
+
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.auth.Auth
+import io.github.jan.supabase.postgrest.Postgrest
+
+object SupabaseClientProvider {
+
+    private const val SUPABASE_URL = "https://veihgdzozylysfzmvjvi.supabase.co"
+    private const val SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlaWhnZHpvenlseXNmem12anZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc5MzYyNDQsImV4cCI6MjEwMzUxMjI0NH0.OgmmACC1_nFVyEDh4dA1ek5N3JlQUJmiRmACq-w2yao"
+
+    val client = createSupabaseClient(
+        supabaseUrl = SUPABASE_URL,
+        supabaseKey = SUPABASE_ANON_KEY
+    ) {
+        install(Auth)
+        install(Postgrest)
+    }
+}
